4c1a6a5ecb3d97b26c4725bcde2a83d0ee4b9530
RedCamel69 <RichardJGilbert@hotmail.com>
2024-05-13 17:52:55 +0100

    Replacing the Event Callback with a service

---
+++ ProjectTracker.Client/Services/ITimeEntryService.cs
@@ -0,0 +1,11 @@
+﻿using ProjectTracker.Shared.Models.TimeEntry;
+
+namespace ProjectTracker.Client.Services
+{
+    public interface ITimeEntryService
+    {
+        event Action? OnChange;
+        public List<TimeEntryResponse> TimeEntries { get; set; }
+        Task GetTimeEntriesByProject(int projectId);
+    }
+}
---
+++ ProjectTracker.Client/Services/TimeEntryService.cs
@@ -0,0 +1,39 @@
+﻿using ProjectTracker.Shared.Models.TimeEntry;
+using System.Net.Http.Json;
+using static System.Net.WebRequestMethods;
+
+namespace ProjectTracker.Client.Services
+{
+    public class TimeEntryService : ITimeEntryService
+    {
+        private readonly HttpClient _http;
+
+        public List<TimeEntryResponse> TimeEntries { get; set; } = new List<TimeEntryResponse>();
+
+        public event Action? OnChange;
+
+        public TimeEntryService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task GetTimeEntriesByProject(int projectId)
+        {
+            List<TimeEntryResponse>? result;
+            if (projectId <= 0)
+            {
+                result = await _http.GetFromJsonAsync<List<TimeEntryResponse>>("api/timeentry");
+            }
+            else
+            {
+                result = await _http.GetFromJsonAsync<List<TimeEntryResponse>>($"api/timeentry/project/{projectId}");
+            }
+
+            if (result != null)
+            {
+                TimeEntries = result;
+                OnChange?.Invoke();
+            }
+        }
+    }
+}
