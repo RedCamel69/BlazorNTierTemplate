4394d9c645bef373d442a77cafa816fcca577e2f
RedCamel69 <RichardJGilbert@hotmail.com>
2024-05-13 14:28:34 +0100

    inject datacontext into repository

--- ProjectTracker.API/Controllers/TimeEntryController.cs
+++ ProjectTracker.API/Controllers/TimeEntryController.cs
@@ -19,15 +19,15 @@ namespace ProjectTracker.API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<List<TimeEntryResponse>> GetAllTimeEntries()
+        public async Task<ActionResult<List<TimeEntryResponse>>> GetAllTimeEntries()
         {
-            return Ok(_timeEntryService.GetAllTimeEntries());
+            return Ok(await _timeEntryService.GetAllTimeEntries());
         }
 
         [HttpGet("{id}")]
-        public ActionResult<TimeEntryResponse> GetTimeEntryById(int id)
+        public async Task<ActionResult<TimeEntryResponse>> GetTimeEntryById(int id)
         {
-            var result = _timeEntryService.GetTimeEntryById(id);
+            var result = await _timeEntryService.GetTimeEntryById(id);
             if (result is null)
             {
                 return NotFound("TimeEntry with the given ID was not found.");
@@ -36,15 +36,15 @@ namespace ProjectTracker.API.Controllers
         }
 
         [HttpPost]
-        public ActionResult<List<TimeEntryResponse>> CreateTimeEntry(TimeEntryCreateRequest timeEntry)
+        public async Task<ActionResult<List<TimeEntryResponse>>> CreateTimeEntry(TimeEntryCreateRequest timeEntry)
         {
-            return Ok(_timeEntryService.CreateTimeEntry(timeEntry));
+            return Ok(await _timeEntryService.CreateTimeEntry(timeEntry));
         }
 
         [HttpPut("{id}")]
-        public ActionResult<List<TimeEntryResponse>> UpdateTimeEntry(int id, TimeEntryUpdateRequest timeEntry)
+        public async Task<ActionResult<List<TimeEntryResponse>>> UpdateTimeEntry(int id, TimeEntryUpdateRequest timeEntry)
         {
-            var result = _timeEntryService.UpdateTimeEntry(id, timeEntry);
+            var result = await _timeEntryService.UpdateTimeEntry(id, timeEntry);
             if (result is null)
             {
                 return NotFound("TimeEntry with the given ID was not found.");
@@ -53,9 +53,9 @@ namespace ProjectTracker.API.Controllers
         }
 
         [HttpDelete("{id}")]
-        public ActionResult<List<TimeEntryResponse>> DeleteTimeEntry(int id)
+        public async Task<ActionResult<List<TimeEntryResponse>>> DeleteTimeEntry(int id)
         {
-            var result = _timeEntryService.DeleteTimeEntry(id);
+            var result = await _timeEntryService.DeleteTimeEntry(id);
             if (result is null)
             {
                 return NotFound("TimeEntry with the given ID was not found.");
