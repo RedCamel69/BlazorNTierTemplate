93e7328e0753bc8ba457b072f5d0e26a32140f69
RedCamel69 <RichardJGilbert@hotmail.com>
2024-05-13 14:32:25 +0100

    convertt DTOs to record structs for performance

--- ProjectTracker.Shared/Models/TimeEntry/TimeEntryCreateRequest.cs
+++ ProjectTracker.Shared/Models/TimeEntry/TimeEntryCreateRequest.cs
@@ -6,10 +6,5 @@ using System.Threading.Tasks;
 
 namespace ProjectTracker.Shared.Models.TimeEntry
 {
-    public class TimeEntryCreateRequest
-    {
-        public required string Project { get; set; }
-        public DateTime Start { get; set; } = DateTime.Now;
-        public DateTime? End { get; set; }
-    }
+    public record struct TimeEntryCreateRequest(string Project, DateTime Start, DateTime? End);
 }
