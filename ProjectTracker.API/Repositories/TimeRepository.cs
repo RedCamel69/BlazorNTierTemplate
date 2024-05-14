06ce6036baabe25d0afd4c6f621e625cf3cceaee
RedCamel69 <RichardJGilbert@hotmail.com>
2024-05-14 14:48:14 +0100

    add user id to other time entry CRUD actions

--- ProjectTracker.API/Repositories/TimeRepository.cs
+++ ProjectTracker.API/Repositories/TimeRepository.cs
@@ -14,7 +14,6 @@ namespace ProjectTracker.API.Repositories
             _userContextService = userContextService;
         }
 
-
         public async Task<List<TimeEntry>> CreateTimeEntry(TimeEntry timeEntry)
         {
             var user = await _userContextService.GetUserAsync();
@@ -33,7 +32,12 @@ namespace ProjectTracker.API.Repositories
 
         public async Task<List<TimeEntry>?> DeleteTimeEntry(int id)
         {
-            var dbTimeEntry = await _context.TimeEntries.FindAsync(id);
+            var userId = _userContextService.GetUserId();
+            if (userId == null)
+                return null;
+
+            var dbTimeEntry = await _context.TimeEntries
+                .FirstOrDefaultAsync(t => t.Id == id && t.User.Id == userId);
             if (dbTimeEntry is null)
             {
                 return null;
@@ -56,20 +60,38 @@ namespace ProjectTracker.API.Repositories
 
         public async Task<List<TimeEntry>> GetTimeEntriesByProject(int projectId)
         {
+            var userId = _userContextService.GetUserId();
+            if (userId == null)
+            {
+                throw new EntityNotFoundException("User was not found.");
+            }
+
             return await _context.TimeEntries
-           .Where(te => te.ProjectId == projectId)
-           .ToListAsync();
+                .Where(te => te.ProjectId == projectId && te.User.Id == userId)
+                .ToListAsync();
         }
 
         public async Task<TimeEntry?> GetTimeEntryById(int id)
         {
-            var timeEntry = await _context.TimeEntries.FindAsync(id);
+            var userId = _userContextService.GetUserId();
+            if (userId == null)
+                return null;
+
+            var timeEntry = await _context.TimeEntries
+                .FirstOrDefaultAsync(t => t.Id == id && t.User.Id == userId);
             return timeEntry;
         }
 
         public async Task<List<TimeEntry>> UpdateTimeEntry(int id, TimeEntry timeEntry)
         {
-            var dbTimeEntry = await _context.TimeEntries.FindAsync(id);
+            var userId = _userContextService.GetUserId();
+            if (userId == null)
+            {
+                throw new EntityNotFoundException("User was not found.");
+            }
+
+            var dbTimeEntry = await _context.TimeEntries
+                .FirstOrDefaultAsync(t => t.Id == id && t.User.Id == userId);
             if (dbTimeEntry is null)
             {
                 throw new EntityNotFoundException($"Entity with ID {id} was not found.");
