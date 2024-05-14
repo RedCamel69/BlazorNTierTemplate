29be3f732b2081f44f36ffbf8a72096c9d6acc2b
RedCamel69 <RichardJGilbert@hotmail.com>
2024-05-14 13:48:33 +0100

    Secure TimeEntryController

--- ProjectTracker.API/Controllers/TimeEntryController.cs
+++ ProjectTracker.API/Controllers/TimeEntryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ProjectTracker.API.Repositories;
 using ProjectTracker.API.Services;
@@ -9,6 +10,7 @@ namespace ProjectTracker.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class TimeEntryController : ControllerBase
     {
         private readonly ITimeEntryService _timeEntryService;
