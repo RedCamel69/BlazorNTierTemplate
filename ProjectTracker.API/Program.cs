676ff4630e9373c87635ac287aa2c3586239546a
RedCamel69 <RichardJGilbert@hotmail.com>
2024-05-14 10:57:03 +0100

    Add Account Countroller Implement Account Service, Registration Method Creates entry in ASpNetUsers

--- ProjectTracker.API/Program.cs
+++ ProjectTracker.API/Program.cs
@@ -43,6 +43,7 @@ builder.Services.AddScoped<ITimeEntryService, TimeEntryService>();
 builder.Services.AddScoped<ITimeEntryRepository, TimeEntryRepository>();
 builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
 builder.Services.AddScoped<IProjectService, ProjectService>();
+builder.Services.AddScoped<IAccountService,AccountService>();
 
 builder.Services.AddCors(options =>
 {
